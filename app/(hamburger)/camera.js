63c90d9b2d9d3b4ef924f41fe42b721d0dcb5037
XiaanB <xiaan_b@hotmail.com>
2025-03-28 12:04:26 +1300

    Fixing the camera, displaying the image.

--- app/(hamburger)/camera.js
+++ app/(hamburger)/camera.js
@@ -1,55 +1,71 @@
-import { Button, StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { Button, StyleSheet, Text, View, Image } from 'react-native';
+import React, {useState} from 'react';
 import * as ImagePicker from 'expo-image-picker';
+import { launchCameraAsync } from 'expo-image-picker';
 
 const CameraScreen = () => {
+    const [pickerImage, setPickerImage] = useState();
+
     async function takeImageHandler() {
-        const { status } = await ImagePicker.requestCameraPermissionsAsync();
-        if (status !== 'granted') {
-            alert('Permission to access camera is required!');
-            return;
-        }
-        const image = await ImagePicker.launchCameraAsync({
+        const image = await launchCameraAsync({
             allowsEditing: true,
+            aspect: [16, 9],
             quality: 0.5,
         });
-        console.log(image);
+        console.log(image.assets[0].uri);
+            if (!image.canceled) {
+                setPickerImage(image.assets[0].uri);
+            }
+        setPickerImage(image.assets[0].uri);
     }
 
     return (
-        <View style={styles.container}>
-            <View style={styles.preview}></View>
-            <Button title='Take a picture' onPress={takeImageHandler} />
-            <Text style={{ marginTop: 10 }}>Camera Screen</Text>
+        <View
+            style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: '#fff',
+                padding: 20,
+                borderWidth: 1,
+                borderColor: 'black',
+                borderRadius: 10,
+                shadowColor: '#000',
+                shadowOffset: { width: 0, height: 2 },
+                shadowOpacity: 0.25,
+                shadowRadius: 3.84,
+                elevation: 5,
+            }}
+        >
+            <View style={{
+                marginBottom: 20,
+                width: '100%',
+                alignItems: 'center',   
+                justifyContent: 'center',
+                borderWidth: 1,
+                borderColor: 'black',
+                borderRadius: 10,   
+                height: 300,
+                backgroundColor: 'lightgray',
+                }}
+            
+            >
+                <Image
+                    source={{ uri: pickerImage }}
+                    style={{ width: '100%', height: 300, borderRadius: 10 }}
+                    resizeMode="cover"
+                    contentFit="cover"
+                    alt="picked-image"
+                ></Image>
+
+            </View>
+        
+
+            <Button title="Take Image" onPress={takeImageHandler} />
         </View>
+        
+            
     );
 }
 
 export default CameraScreen;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderWidth: 1,
-        borderColor: 'black',
-        borderRadius: 10,
-        padding: 20,
-        margin: 10,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.25,
-    },
-    preview: {
-        width: '100%',
-        height: 250,
-        backgroundColor: 'lightgrey',
-        borderRadius: 10,
-        borderWidth: 1,
-        borderColor: 'black',
-        marginBottom: 10,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-    }
-});
