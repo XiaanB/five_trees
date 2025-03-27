3e1620b17d3afd99a79e105f987cbd2a91b8d15d
XiaanB <xiaan_b@hotmail.com>
2025-03-28 10:45:25 +1300

    Adding Google Maps to the Contact Us page.

--- app/(hamburger)/contactUs.js
+++ app/(hamburger)/contactUs.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import MapView, { Circle, Marker } from 'react-native-maps';
+import { useNavigation } from '@react-navigation/native';
 
 export default function AboutUsScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Contact Us Screen</Text>
-    </View>
+    <View style={{ flex: 1 }}>
+      <MapView style={{ flex: 1 }}
+        initialRegion={{
+      
+          latitude: -43.50257416921142,
+          longitude: 172.58669100802567,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker
+          coordinate={{ latitude: -43.50257416921142, longitude: 172.58669100802567 }}
+          title="Five Trees Ltd"
+          description="119 Brookside Terrace, Bryndwr, Christchurch 8053"        
+        /> 
+
+        <Circle
+          center={{ latitude: -43.50257416921142, longitude: 172.58669100802567 }}
+          radius={1000}
+          strokeColor="rgba(255, 0, 0, 0.5)"
+          fillColor="rgba(255, 0, 0, 0.2)"  
+        />
+      </MapView>
+      <Text style={{ position: 'absolute', bottom: 20, left: 20 }}>
+        Contact Us
+      </Text>
+    </View>    
   );
 }
